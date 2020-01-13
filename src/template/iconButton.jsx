179413fe9f139ce58abe0eb8924d7e1fbdec6f66
Rodrik Klesse <r.klesse@sidi.org.br>
2020-01-13 13:34:21 -0300

    Metodo adicionar, refresh, excluir e a renderização da lista implementados

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import If from './if'
 
-export default props => {
-    if(props.hide){
-        return null
-    } else {
-        return (
-            <button className={'btn btn-'+ props.style}
+export default props => (
+    // Primeiro exmplo de solução
+    // if(props.hide){
+    //     return null
+    // } else {
+    //     return (
+    // <button className={'btn btn-'+ props.style}
+    // onClick={props.onClick}>
+    //     <i className={'fa fa-'+ props.icon}></i>
+    // </button>
+    //     )
+    // }
+
+    <If test={!props.hide}>
+        <button className={'btn btn-'+ props.style} 
             onClick={props.onClick}>
-                <i className={'fa fa-'+ props.icon}></i>
-            </button>
-        )
-    }
-}
+            <i className={'fa fa-'+ props.icon}></i>
+        </button>
+    </If>
+)
